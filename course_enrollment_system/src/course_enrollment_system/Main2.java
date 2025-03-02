06ef284e10de6621f9c35b87c283f471203c5f73
Ramchilakala <ramchi4ever@gmail.com>
2025-03-02 13:39:19 +0530

    Main2 class is added in main2 branch

---
+++ course_enrollment_system/src/course_enrollment_system/Main2.java
@@ -0,0 +1,13 @@
+package course_enrollment_system;
+
+public class Main2 {
+int a ;
+int b;
+public Main2(int a, int b) {
+	this.a = a;
+	this.b = b;
+	}
+public static void main3(int a , int b) {
+	System.out.println(a+b);
+}
+}
